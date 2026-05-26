a4cc8c1dec070041004730a759f0bcd7da2e8956
Guillen40 <guillenkevin442@gmail.com>
2026-05-26 17:13:15 -0300

    .

---
+++ Tarea 2/Programa de loteria/Programa de loteria/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Programa_de_loteria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tBox1 = new System.Windows.Forms.TextBox();
+            this.tBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tBox4 = new System.Windows.Forms.TextBox();
+            this.tBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tBox6 = new System.Windows.Forms.TextBox();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(40)))), ((int)(((byte)(217)))));
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(16)))), ((int)(((byte)(101)))));
+            this.btn1.Location = new System.Drawing.Point(12, 103);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(229, 75);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "JUGAR!!!";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(85)))), ((int)(((byte)(154)))));
+            this.label1.Location = new System.Drawing.Point(335, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Numero jugado";
+            // 
+            // tBox1
+            // 
+            this.tBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(23)))), ((int)(((byte)(42)))));
+            this.tBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tBox1.ForeColor = System.Drawing.Color.White;
+            this.tBox1.Location = new System.Drawing.Point(332, 62);
+            this.tBox1.Name = "tBox1";
+            this.tBox1.Size = new System.Drawing.Size(145, 29);
+            this.tBox1.TabIndex = 3;
+            this.tBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tBox2
+            // 
+            this.tBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(23)))), ((int)(((byte)(42)))));
+            this.tBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tBox2.ForeColor = System.Drawing.Color.White;
+            this.tBox2.Location = new System.Drawing.Point(332, 149);
+            this.tBox2.Name = "tBox2";
+            this.tBox2.Size = new System.Drawing.Size(145, 29);
+            this.tBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(85)))), ((int)(((byte)(154)))));
+            this.label2.Location = new System.Drawing.Point(329, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Dinero apostado";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(158)))), ((int)(((byte)(212)))));
+            this.label3.Location = new System.Drawing.Point(108, 270);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "PRIMERA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(158)))), ((int)(((byte)(212)))));
+            this.label4.Location = new System.Drawing.Point(210, 270);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 24);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "SEGUNDA";
+            // 
+            // tBox3
+            // 
+            this.tBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(23)))), ((int)(((byte)(42)))));
+            this.tBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(158)))), ((int)(((byte)(212)))));
+            this.tBox3.Location = new System.Drawing.Point(346, 297);
+            this.tBox3.Name = "tBox3";
+            this.tBox3.ReadOnly = true;
+            this.tBox3.Size = new System.Drawing.Size(41, 29);
+            this.tBox3.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(158)))), ((int)(((byte)(212)))));
+            this.label5.Location = new System.Drawing.Point(318, 270);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 24);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "TERCERA";
+            // 
+            // tBox4
+            // 
+            this.tBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(23)))), ((int)(((byte)(42)))));
+            this.tBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tBox4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(158)))), ((int)(((byte)(212)))));
+            this.tBox4.Location = new System.Drawing.Point(243, 297);
+            this.tBox4.Name = "tBox4";
+            this.tBox4.ReadOnly = true;
+            this.tBox4.Size = new System.Drawing.Size(41, 29);
+            this.tBox4.TabIndex = 12;
+            // 
+            // tBox5
+            // 
+            this.tBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(23)))), ((int)(((byte)(42)))));
+            this.tBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tBox5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(158)))), ((int)(((byte)(212)))));
+            this.tBox5.Location = new System.Drawing.Point(137, 297);
+            this.tBox5.Name = "tBox5";
+            this.tBox5.ReadOnly = true;
+            this.tBox5.Size = new System.Drawing.Size(41, 29);
+            this.tBox5.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(158)))), ((int)(((byte)(212)))));
+            this.label6.Location = new System.Drawing.Point(219, 354);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 24);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "PREMIO";
+            // 
+            // tBox6
+            // 
+            this.tBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(191)))), ((int)(((byte)(36)))));
+            this.tBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tBox6.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.tBox6.Location = new System.Drawing.Point(195, 381);
+            this.tBox6.Name = "tBox6";
+            this.tBox6.ReadOnly = true;
+            this.tBox6.Size = new System.Drawing.Size(136, 29);
+            this.tBox6.TabIndex = 15;
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(40)))), ((int)(((byte)(217)))));
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(16)))), ((int)(((byte)(101)))));
+            this.btn2.Location = new System.Drawing.Point(12, 12);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(193, 79);
+            this.btn2.TabIndex = 16;
+            this.btn2.Text = "NUEVA JUGADA";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label7.Location = new System.Drawing.Point(12, 222);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 22);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Resultados:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(46)))));
+            this.ClientSize = new System.Drawing.Size(532, 447);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.tBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tBox5);
+            this.Controls.Add(this.tBox4);
+            this.Controls.Add(this.tBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "🎰 Ludopatas Anonimos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tBox1;
+        private System.Windows.Forms.TextBox tBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tBox4;
+        private System.Windows.Forms.TextBox tBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tBox6;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Tarea 2/Programa de loteria/Programa de loteria/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programa_de_loteria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            // Generador de numeros random
+            Random random = new Random();
+
+            // validacion de numero
+            if (!int.TryParse(tBox1.Text, out int numeroJugado) || numeroJugado < 1 || numeroJugado > 99)
+            {
+                MessageBox.Show("Por favor, ingresa un numero valido del 1 al 99.", "Error de numero");
+                return;
+            }
+
+            if (!decimal.TryParse(tBox2.Text, out decimal dineroApostado) || dineroApostado <= 0)
+            {
+                MessageBox.Show("Por favor, ingresa una cantidad de dinero valida.", "Error de dinero");
+                return;
+            }
+
+            // Donde aparecen los numeros
+            int primera = random.Next(1, 100);
+            int segunda = random.Next(1, 100);
+            int tercera = random.Next(1, 100);
+            tBox5.Text = primera.ToString();
+            tBox4.Text = segunda.ToString();
+            tBox3.Text = tercera.ToString();
+
+            // multiplicacion del premio
+            decimal premio = 0;
+
+            if (numeroJugado == primera)
+            {
+                premio = dineroApostado * 1000;
+            }
+            else if (numeroJugado == segunda)
+            {
+                premio = dineroApostado * 100;
+            }
+            else if (numeroJugado == tercera)
+            {
+                premio = dineroApostado * 10;
+            }
+
+            tBox6.Text = premio.ToString("C");
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            // limpiar entradas
+            btn1.Text = "";
+            btn1.Text = "";
+
+            // Limpiar casillas
+            tBox5.Text = "";
+            tBox2.Text = "";
+            tBox3.Text = "";
+
+            // Limpiar el premio
+            tBox6.Text = "";
+        }
+    }
+}
